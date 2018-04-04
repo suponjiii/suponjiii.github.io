35871f32e33c024154654e76f1542f5179d60265
Stefan <stefanleminh@gmail.com>
2018-04-05 00:33:55 +0200

    Fix scrolling for good, add icons to contacts

--- scripts/main.js
+++ scripts/main.js
@@ -3,26 +3,7 @@ $(document).ready(function(){
     sr.reveal("section");
 })
 
-$(function() {
 
-    var $sidebar   = $("#sidebar"), 
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        topPadding = 150;
-
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
-            }, 0);
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            }, 0);
-        }
-    });
-    
-});
 
 
 var menu = document.querySelector('#sidebar');
