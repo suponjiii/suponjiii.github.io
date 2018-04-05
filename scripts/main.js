8fb3aefee37554632608d0f89c4080913d67bea0
Stefan <stefanleminh@gmail.com>
2018-04-05 18:16:07 +0200

    Change colors, make navbar work

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
     window.sr = ScrollReveal();
     sr.reveal(".scroll");
+    $('.card-header-icon').click(function(){
+        $(this).parent().siblings().slideToggle("slow");
+        $(this).toggleClass("activeToggle")
+    });
+    $('.navbar-burger').click(function(){
+        $(this).toggleClass("is-active");
+    })
 })
 
 
 
 
+
 var menu = document.querySelector('#sidebar');
+var navbar = document.querySelector(".navbar-start");
 scrollSpy(menu, 2000, 'easeInOutQuint');
+scrollSpy(navbar, 2000, 'easeInOutQuint');
+
