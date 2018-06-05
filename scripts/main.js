8ecb0e7885ecf43e4e16fb8c9f1bc972b3f4973f
michael <michael.staerke@mail.de>
2018-06-05 22:54:00 +0200

    Read html input-values, develop function prototypes, start solo calculation

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,43 @@
+// Global Variables
+
+// Variables declaration
+var amountAdditionalLines, amountOfChorusLines, 
+    acoustic, pop, rock, electro, 
+    soloTuningBase, soloTimingBase, soloMixingBase, soloDiscount,
+    duetTuningBase, duetTimingBase, duetMixingBase, duetDiscount, 
+    trioTuningBase, trioTimingBase, trioMixingBase, trioDiscount;
+
+// Mixing prices
+var amountAdditionalLines = 0,
+    amountOfChorusLines = 0,
+    acoustic = 1,
+    pop = 1.05,
+    rock = 1.2,
+    electro = 1.5;
+    other = 1;
+
+// Solo
+var soloTuningBase = 7,
+    soloTimingBase = 5,
+    soloMixingBase = 10,
+    soloDiscount = 0.9;
+
+// Duet
+var duetTuningBase = 8,
+    duetTimingBase = 9,
+    duetMixingBase = 12,
+    duetDiscount = 0.85;
+
+// Trio
+var trioTuningBase = 9,
+    trioTimingBase = 11,
+    trioMixingBase = 17,
+    trioDiscount = 0.8;
+
+// Chorus
+
+
+
 $(document).ready(function(){
     window.sr = ScrollReveal();
     sr.reveal(".scroll");
@@ -40,6 +80,32 @@ $(document).ready(function(){
         }
         });
     
+        
+    $('#calculation-button').click(function(){
+        //Hole Werte von den Input-Feldern
+        var typeOfMix = $('#typeOfMix').val();  
+        var genre = $('#genre').val();
+        var amountOfLines = $('#amountOfLines').val();
+        var amountOfAdditionalLines = $('#amountOfAdditionalLines').val();
+        var more = $('#more').val();
+        //Überprüfe Art des Mixes und rufe passende Funktionen auf (mit passenden Parametern)
+        if (amountOfLines === "Solo"){
+            
+            solo(typeOfMix, amountOfAdditionalLines, genre);
+            
+        } else if (amountOfLines === "Duet") {
+            
+            duet();
+            
+        } else if (amountOfLines === "Trio") {
+            
+            trio();
+            
+        } else {
+            
+            chorus();
+        } 
+    });
 })
 
 
@@ -49,57 +115,51 @@ scrollSpy(menu, 2000, 'easeInOutQuint');
 scrollSpy(navbar, 2000, 'easeInOutQuint');
 
 
-
-
-
-// Variables declaration
-var amountAdditionalLines, amountOfChorusLines, 
-    acoustic, pop, rock, electro, 
-    soloTuningBase, soloTimingBase, soloMixingBase, soloDiscount,
-    duetTuningBase, duetTimingBase, duetMixingBase, duetDiscount, 
-    trioTuningBase, trioTimingBase, trioMixingBase, trioDiscount;
-
-
-// var amountOfLines_selection= new Array();
-// amountOfLines_selection["Solo"]=;
-
-
-
-// Mixing prices
-var amountAdditionalLines = input,
-    amountOfChorusLines = input,
-    acoustic = 1,
-    pop = 1,
-    rock = 1.2,
-    electro = 1.5;
-
-// Solo
-var soloTuningBase = 7,
-    soloTimingBase = 5,
-    soloMixingBase = 10,
-    soloDiscount = 0.9;
-
-// Duet
-var duetTuningBase = 8,
-    duetTimingBase = 9,
-    duetMixingBase = 12,
-    duetDiscount 0.85;
-
-// Trio
-var trioTuningBase = 9,
-    trioTimingBase = 11,
-    trioMixingBase = 17,
-    trioDiscount = 0.8;
-
-// Chorus
-
-
-
-function get() {
+function solo(typeOfMix, amountOfAdditionalLines, genre) {
+    var soloTuningFinal,
+        soloTimingFinal,
+        soloMixingFinal;
+    //Überprüfe Genre und setze den Modifier passend
+    if (genre === "Acoustic") {
+        genreModifier = acoustic;
+    } else if (genre = "Rock") {
+        genreModifier = rock;
+    } else if (genre = "Pop") {
+        genreModifier = pop;
+    } else if (genre = "Electro") {
+        genreModifier = electro;
+    } else {
+        genreModifier = other;
+    }
     
-    
+    //Überprüfe Typ des Mixes und setze Werte passend
+    if(typeOfMix === "TuningOnly") {
+        soloTimingFinal = 0;
+        soloMixingFinal = 0;
+        soloTuningFinal = soloTuningBase;
+        soloDiscountFinal = 1;
+    } else if(typeOfMix === "TimingOnly") {
+        //TODO: Setze Variablen wie oben aber mit den korrekten Werten
+    } else if(typeOfMix === "MixingOnly") {
+        //TODO: Setze Variablen wie oben aber mit den korrekten Werten
+    } else {
+        //TODO: Setze Variablen wie oben aber mit den korrekten Werten
+    }
+    //Setze Result-Feld mit dem berechneten Wert
+     $('#result-field').val(((soloTuningFinal + soloTimingFinal + soloMixingFinal + amountOfAdditionalLines) * genreModifier ) * soloDiscountFinal);
 }
 
+function duet() {
+    //TODO: Implementiere Berechnung ähnlich wie solo
+}
+
+function trio() {
+    //TODO: Implementiere Berechnung ähnlich wie solo
+}
+
+function chorus() {
+    //TODO: Implementiere Berechnung ähnlich wie solo
+}
 
 
 
